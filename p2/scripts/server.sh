25120d639720bb49d5f7d112a93e5cf643fadd99
nils <nosterme@students.42wolfsburg.de>
2025-03-18 13:41:50 +0100

    restructure of apps yaml files and static html with configmaps

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -6,4 +6,4 @@ export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip $1"
 
 wget -qO - https://get.k3s.io | sh
 
-kubectl apply -f /vagrant/confs/deployment.yaml
+kubectl apply -Rf /vagrant/confs/apps
