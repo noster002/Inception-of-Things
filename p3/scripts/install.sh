21a47cc43d64fa6d595cf59be37bc1b186417f47
nosterme <nosterme@student.42wolfsburg.de>
2025-03-25 10:06:07 -0400

    more explicit run script

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -20,7 +20,7 @@ installDocker() {
 	| runAsRoot gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	# verify the fingerprint of the key
 	# gpg --no-default-keyring --keyring /usr/share/keyrings/docker-archive-keyring.gpg --fingerprint
-	# should have the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
+	# should have the fingerprint 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
 	echo "Types: deb
 URIs: https://download.docker.com/linux/debian
 Suites: $(lsb_release -cs)
@@ -38,7 +38,7 @@ installKubectl() {
 	| runAsRoot gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
 	# verify the fingerprint of the key
 	# gpg --no-default-keyring --keyring /usr/share/keyrings/kubernetes-archive-keyring.gpg --fingerprint
-	# should have the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
+	# should have the fingerprint DE15 B144 86CD 377B 9E87  6E1A 2346 54DA 9A29 6436 [expires: 2026-12-29]
 	echo "Types: deb
 URIs: https://pkgs.k8s.io/core:/stable:/v1.32/deb/
 Suites: /
--- p3/scripts/run.sh
+++ p3/scripts/run.sh
@@ -2,16 +2,16 @@
 
 k3d cluster create nosterme -p 8888:30888@server:0
 
-kubectl create namespace dev
-kubectl create namespace argocd
+kubectl create namespace dev && kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl wait --for condition=Ready pod --all -n argocd --timeout 120s
+kubectl apply -f confs/application.yaml -n argocd
 
 kubectl port-forward service/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
-
+echo "Go to https://localhost:8080 to access argoCD"
+echo ""
+echo "ArgoCD user credentials:"
+echo "username: admin"
 PASSWORD=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode)
-argocd login --username admin --password $PASSWORD localhost:8080 --insecure
-argocd account update-password --current-password $PASSWORD --new-password bookworm
-
-kubectl apply -f confs/deployment.yaml -n argocd
+echo "password: $PASSWORD"
